cb41697805c71a14682c696b295279d0ef5bd2cd
David <dkulakowski1999@gmail.com>
2023-04-12 09:49:00 +0200

    zmiana jakas chyba

--- Guiwindows/ViewModel/WinVM.cs
+++ Guiwindows/ViewModel/WinVM.cs
@@ -10,7 +10,6 @@ namespace Guiwindows.ViewModel
     public class WinVM : MVVMKit.ViewModel
     {
         private readonly WinModel model;
-
         public bool NotConnected { get; private set; }
         public bool Connected { get; private set; }
 
@@ -19,9 +18,7 @@ namespace Guiwindows.ViewModel
             model = WinModel.Instance;
             model.Alert += (s, m) => App.AlertServices.AlertAsync("Connection", m, "ok");
             model.ConnectionAttempt += (s, m) => DisplayIP();
-
             model.Connection.ConnectionStateChanged += ConnectionStateChanged;
-
             model.Start();
             Application.Current.MainPage.Appearing += (s, a) => Task.Run(DisplayIP);
         }
